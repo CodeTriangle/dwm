7b43e65de7edebb97a3ebd77f8bb4614ca6e2d8e
Reuben Staley <lighthousemaniac@gmail.com>
2021-09-02 05:35:47 +0000

    fixing some (percieved) inelegancies in my dual-monitor workflow

--- dwm.c
+++ dwm.c
@@ -841,11 +841,11 @@ drawbar(Monitor *m)
 	Client *c;
 
 	/* draw status first so it can be overdrawn by tags later */
-	if (m == selmon) { /* status is only drawn on selected monitor */
+	// if (m == selmon) { /* status is only drawn on selected monitor */
 		drw_setscheme(drw, scheme[SchemeNorm]);
 		tw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
 		drw_text(drw, m->ww - tw, 0, tw, bh, 0, stext, 0);
-	}
+	// }
 
 	for (c = m->clients; c; c = c->next) {
 		occ |= c->tags;
@@ -1625,7 +1625,7 @@ sendmon(Client *c, Monitor *m)
 	detach(c);
 	detachstack(c);
 	c->mon = m;
-	c->tags = m->tagset[m->seltags]; /* assign tags of target monitor */
+	// c->tags = m->tagset[m->seltags]; /* assign tags of target monitor */
 	attach(c);
 	attachstack(c);
 	focus(NULL);
